1aee5d45708a908c70351a1fee3dbf07988cadf7
Orion68 <orions68@gmail.com>
2024-01-27 18:55:37 +0000

    Update 27-01-2024 18:55

--- Programación/Algoritmos Raros/Algoritmo Patrick/obj/Debug/net8.0/Algoritmo Patrick.AssemblyInfo.cs
+++ Programación/Algoritmos Raros/Algoritmo Patrick/obj/Debug/net8.0/Algoritmo Patrick.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb3d7f5f1aad3b30502ca3393e0060aeb11f135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205cbebe304428fc17a6f5493178bf5419dcab3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programación/Algoritmos Raros/Determinante/obj/Debug/net8.0/Determinante.AssemblyInfo.cs
+++ Programación/Algoritmos Raros/Determinante/obj/Debug/net8.0/Determinante.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Determinante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb3d7f5f1aad3b30502ca3393e0060aeb11f135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205cbebe304428fc17a6f5493178bf5419dcab3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Determinante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Determinante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
