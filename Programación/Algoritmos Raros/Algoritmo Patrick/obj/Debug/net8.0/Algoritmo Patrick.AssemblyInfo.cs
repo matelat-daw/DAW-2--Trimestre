d6736d588d5c9178aa9ab559e354bd487d141edd
Orion68 <orions68@gmail.com>
2024-01-27 23:04:22 +0000

    Update 27-01-2024 23:05

--- Programación/Algoritmos Raros/Algoritmo Patrick/obj/Debug/net8.0/Algoritmo Patrick.AssemblyInfo.cs
+++ Programación/Algoritmos Raros/Algoritmo Patrick/obj/Debug/net8.0/Algoritmo Patrick.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205cbebe304428fc17a6f5493178bf5419dcab3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32da3cbdfabd68f22022ea16ffe4f6c3d84d48a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programación/Algoritmos Raros/Sarrus Rule/obj/Debug/net8.0/Sarrus Rule.AssemblyInfo.cs
+++ Programación/Algoritmos Raros/Sarrus Rule/obj/Debug/net8.0/Sarrus Rule.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sarrus Rule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205cbebe304428fc17a6f5493178bf5419dcab3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32da3cbdfabd68f22022ea16ffe4f6c3d84d48a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sarrus Rule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sarrus Rule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
