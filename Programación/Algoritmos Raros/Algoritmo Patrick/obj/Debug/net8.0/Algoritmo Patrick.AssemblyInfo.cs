514bebb142989f72585dd273e83e6f43f8378a0a
Orion68 <orions68@gmail.com>
2024-02-06 15:04:49 +0000

    Update 06-02-2024 15:05

--- Programación/Algoritmos Raros/Algoritmo Patrick/obj/Debug/net8.0/Algoritmo Patrick.AssemblyInfo.cs
+++ Programación/Algoritmos Raros/Algoritmo Patrick/obj/Debug/net8.0/Algoritmo Patrick.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04652bcc8cae938ce942b89332d42e3e4843d679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d23b99fae60fdb5bc2f31965fcbe6f978a4e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algoritmo Patrick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
