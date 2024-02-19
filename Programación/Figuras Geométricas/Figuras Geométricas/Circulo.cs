e91811d2f154f184f3b103fb47f47aeb5d1d7eb6
Orion68 <orions68@gmail.com>
2024-02-19 23:49:08 +0000

    Update 19-02-2024 23:50

---
+++ Programación/Figuras Geométricas/Figuras Geométricas/Circulo.cs
@@ -0,0 +1,51 @@
+﻿public class Circulo
+{
+    private int radio;
+    private Point posicion;
+
+    public Circulo (int x, int y, int radio)
+    {
+        posicion = new Point();
+        posicion.setX(x);
+        posicion.setY(y);
+        this.radio = radio;
+    }
+
+    public int getRadio()
+    {
+        return radio;
+    }
+
+    public double perimetro()
+    {
+        return 2 * Math.PI * radio;
+    }
+
+    public double area()
+    {
+        return Math.PI * radio * radio;
+    }
+
+    public void mostrar()
+    {
+        double thickness = 0.4;
+        double rIn = radio - thickness, rOut = radio + thickness;
+
+        for (double y = radio; y >= -radio; --y)
+        {
+            for (double x = -radio; x < rOut; x += 0.5)
+            {
+                double value = x * x + y * y;
+                if (value >= rIn * rIn && value <= rOut * rOut)
+                {
+                    Console.Write('*');
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Programación/Figuras Geométricas/Figuras Geométricas/Rectangle.cs
@@ -0,0 +1,79 @@
+﻿
+public class Rectangle
+{
+    private int ancho;
+    private int alto;
+    private Point posicion;
+
+    public Rectangle (int x, int y, int ancho, int alto)
+    {
+        posicion = new Point();
+        posicion.setX(x);
+        posicion.setY(y);
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public int getAncho()
+    {
+        return ancho;
+    }
+
+    public int getAlto()
+    {
+        return alto;
+    }
+
+    public static void pruebaRectangulo()
+    {
+        int area_total;
+        Rectangle[] array = [
+            new Rectangle(0, 0, 10, 10),
+            new Rectangle(5, 5, 15, 15),
+            new Rectangle(10, 10, 20, 20)
+            ];
+        area_total = areas(array);
+        Console.WriteLine("\nEl Area de Todos Los Rectángulos es: {0}\n", area_total);
+    }
+
+    private static int areas(Rectangle[] array)
+    {
+        int result = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            result += array[i].area();
+        }
+        return result;
+    }
+
+    public void mostrar()
+    {
+        for (int i = posicion.getX(); i <= ancho + posicion.getX(); i++)
+        {
+            Console.SetCursorPosition(i, 0 + posicion.getY());
+            Console.Write("*");
+            Console.SetCursorPosition(i, alto + posicion.getY());
+            Console.Write("*");
+        }
+
+        for (int j = posicion.getY(); j < alto + posicion.getY(); j++)
+        {
+            Console.SetCursorPosition(posicion.getX(), j);
+            Console.Write("*");
+            Console.SetCursorPosition(ancho + posicion.getX(), j);
+            Console.Write("*");
+        }
+        Console.WriteLine("\n");
+    }
+
+    public int perimetro()
+    {
+        return ancho * 2 + alto * 2;
+    }
+
+    public int area()
+    {
+        return ancho * alto;
+    }
+}
