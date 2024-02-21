dd0ca1ddd77bee3ad5491bc946d1a780d990ce13
matelat-daw <cesarmatelat@gmail.com>
2024-02-21 09:19:20 +0000

    Update 21-02-2024 9:20

--- Programación/Orientado a Objetos/Candado/obj/Debug/net8.0/Candado.AssemblyInfo.cs
+++ Programación/Orientado a Objetos/Candado/obj/Debug/net8.0/Candado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Candado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d276d66f1da88454bbeb895efeec3bc48c201e2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c722073cb5455ccbb1ab2ccfc88108a8661c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Candado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Candado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
