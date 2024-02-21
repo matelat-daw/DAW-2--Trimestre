b0703611e96ac46c6c487fa19cc3c19d6cca8b94
matelat-daw <cesarmatelat@gmail.com>
2024-02-21 12:27:43 +0000

    Update Again

--- Programación/Orientado a Objetos/Lista Enlazada/obj/Debug/net8.0/Lista Enlazada.AssemblyInfo.cs
+++ Programación/Orientado a Objetos/Lista Enlazada/obj/Debug/net8.0/Lista Enlazada.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista Enlazada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c722073cb5455ccbb1ab2ccfc88108a8661c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0ca1ddd77bee3ad5491bc946d1a780d990ce13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista Enlazada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista Enlazada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
