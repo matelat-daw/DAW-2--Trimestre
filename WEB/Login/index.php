e4911a738a97619e2a2ca2728c83066a7f8ac1a4
Orion68 <orions68@gmail.com>
2024-03-03 17:54:28 +0000

    Update 03-03-2024 17:55

--- WEB/Login/index.php
+++ WEB/Login/index.php
@@ -12,7 +12,7 @@ include "includes/modal.html";
                     <div class="row">
                         <div class="col-md-7">
                         <br><br><br>
-                            <h2>Te Damos la Bienvenida a la WEB de la Peluquería de Javier Borneo</h2>
+                            <h2>Página de Registo y Login de Usuarios</h2>
                             <br>
                             <h3>Registro de Clientes</h3>
                             <br>
@@ -44,7 +44,7 @@ include "includes/modal.html";
                         </div>
                         <div class="col-md-5">
                             <br><br><br><br><br><br><br>
-                            <h3>Entrada de Clientes para Solicitar un Turno</h3>
+                            <h3>Entrada de Usuarios</h3>
                             <br>
                             <form action="login.php" method="post">
                                 <label><input type="email" name="email" required> E-mail</label>
