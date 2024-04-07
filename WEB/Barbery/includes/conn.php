5abd46e74e6a760c20b39f6664da3ce6cc47cc46
Orion68 <orions68@gmail.com>
2024-04-07 17:49:00 +0100

    Update 07-04-2024 17:50

--- WEB/Barbery/includes/conn.php
+++ WEB/Barbery/includes/conn.php
@@ -2,7 +2,7 @@
 session_start(); // Incluyo el session_start() ya que se usará en casi todos los scripts.
 try // Intenta la conexión
 {
-	$conn = new PDO('mysql:host=localhost;dbname=barbery', "root", getenv('MySQL'));
+	$conn = new PDO('mysql:host=localhost;dbname=barbery', "root", $_ENV['MySQL']);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) // En caso de error
