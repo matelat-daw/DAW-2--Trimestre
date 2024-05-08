b503043710ddc54bb1002808ce0b38c75fd7ccfa
Orion68 <orions68@gmail.com>
2024-05-08 16:47:53 +0100

    Update 08-05-2024 16:47

--- Lãberit/TEST/index.php
+++ Lãberit/TEST/index.php
@@ -1,4 +1,23 @@
 <?php
-$time = date("U",strtotime('2024-05-06T15:40:34Z'));
-echo $time;
+// $time = date("U",strtotime('2024-05-06T15:40:34Z'));
+// echo $time;
+
+echo '<script>
+    let array = [];
+    array = [{length: 10, qtty: 3}, {length: 20, qtty: 1}, {length: 30, qtty: 1}, {length: 25, qtty: 2}, {length: 15, qtty: 1}];
+
+    array.sort(function (a, b)
+    {
+        return b.qtty - a.qtty || b.length - a.length;
+    });
+
+    let data = [];
+    for (i = 0; i < array.length; i++)
+    {
+        data[i] = [array[i].length, array[i].qtty];
+    }
+
+    console.log("Los Datos Ahora son: " + data);
+
+</script>';
 ?>
