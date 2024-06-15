8038f46cc4fff4bc3f024210f48925778d6056cc
Orion68 <orions68@gmail.com>
2024-06-15 11:51:55 +0100

    Update 15-06-2024 11:51

--- Marcado/JavaScript/Ejercicio-7/js/script.js
+++ Marcado/JavaScript/Ejercicio-7/js/script.js
@@ -1,4 +1,4 @@
-let imgs = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg"];
+let imgs = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg", "img/img7.jpg", "img/img8.jpg", "img/img9.jpg", "img/img10.jpg", "img/img11.jpg"];
 
 function show()
 {
