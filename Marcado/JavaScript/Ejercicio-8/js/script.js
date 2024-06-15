d12ae5157dcdc6996e71467d9f025c476323ea7a
Orion68 <orions68@gmail.com>
2024-06-16 00:30:23 +0100

    Update 16-06-2024 0:30

--- Marcado/JavaScript/Ejercicio-8/js/script.js
+++ Marcado/JavaScript/Ejercicio-8/js/script.js
@@ -11,13 +11,19 @@ function draw()
     for (var i = 0; i < rowcol; i++)
     {
         let row = document.createElement("tr");
+        row.id = i + 1;
         myTable.appendChild(row);
         for (var j = 0; j < rowcol; j++)
         {
             let col = document.createElement("td");
+            col.id = j + 1;
             if (i == 0 && j == 0)
             {
-                col.id = "circle";
+                col.setAttribute("class", "circle");
+                // let div = document.createElement("span");
+                // div.setAttribute("class", "circle");
+                // div.setAttribute("style", "display: inline-block; width:" + 600 / rowcol + "px; height:" + 600 / rowcol + "px;");
+                // col.appendChild(div);
             }
             row.appendChild(col);
         }
@@ -42,5 +48,14 @@ function down()
 
 function up()
 {
+    let tr = document.getElementsByTagName("tr");
+    let td = document.getElementByTagName("td");
+    
+    if (tr.childNodes[0])
+    {
+        if (td.childNodes[0])
+        {
 
+        }
+    }
 }
