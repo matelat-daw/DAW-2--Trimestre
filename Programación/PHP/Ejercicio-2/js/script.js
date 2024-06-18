72536416233a7ce1ecab52afe363e4eb47c93d12
Orion68 <orions68@gmail.com>
2024-06-18 23:48:24 +0100

    Update 18-06-2024 23:48

--- Programación/PHP/Ejercicio-2/js/script.js
+++ Programación/PHP/Ejercicio-2/js/script.js
@@ -34,3 +34,90 @@ function ej1Opera()
     "Multiplicación:   <br>" + "<span class='margin'>" + num1 + " * " + num2 + " = " + num1 * num2 + "<br><br>" +
     "División:         <br>" + "<span class='margin'>" + num1 + " / " + num2 + " = " + num1 / num2;
 }
+
+function toast(warn, ttl, msg) // Función para mostrar el Dialogo con los mensajes de alerta, recibe, Código, Título y Mensaje.
+{
+    var alerta = document.getElementById("alerta"); // La ID del botón del dialogo.
+    var title = document.getElementById("title"); // Asigno a la variable title el h4 con id title.
+    var message = document.getElementById("message"); // Asigno a la variable message el h5 con id message;
+    if (warn == 1) // Si el código es 1, es una alerta.
+    {
+        title.style.backgroundColor = "#000000"; // Pongo los atributos, color de fondo negro.
+        title.style.color = "yellow"; // Y color del texto amarillo.
+    }
+    else if (warn == 0) // Si no, si el código es 0 es un mensaje satisfactorio.
+    {
+        title.style.backgroundColor = "#FFFFFF"; // Pongo los atributos, color de fondo blanco.
+        title.style.color = "blue"; // Y el color del texto azul.
+    }
+    else // Si no, viene un 2, es una alerta de error.
+    {
+        title.style.backgroundColor = "#000000"; // Pongo los atributos, color de fondo negro.
+        title.style.color = "red"; // Y color del texto rojo.
+    }
+    title.innerHTML = ttl; // Muestro el Título del dialogo.
+    message.innerHTML = msg; // Muestro los mensajes en el diálogo.
+    alerta.click(); // Lo hago aparecer pulsando el botón con ID alerta.
+}
+
+function showEye(which) // Función para mostrar el ojo de los input de las contraseñas, recibe el número del elemento que contiene el ojo.
+{
+    let eye = document.getElementById("togglePassword" + which); // Asigno a eye la id del elemento que contiene el ojo.
+    eye.style.visibility = "visible"; // Hago visible el elemento, el ojo.
+}
+
+function spy(which) // Función para el ojito de las Contraseñas al hacer click en el ojito, recibe el número de la ID del input de la password.
+{
+    const togglePassword = document.querySelector('#togglePassword' + which); // Asigno a la constante togglePassword el input con ID togglePassword + which.
+    const password = document.querySelector('#pass' + which); // Asigno a password la ID del input con ID pass + which.
+    
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password'; // Asigno a type el resultado de un operador ternario, si presiono el ojito y el tipo del input es password
+    // lo cambia a text, si es text lo cambia a password.
+    password.setAttribute('type', type); // Le asigno el atributo al input password.
+    togglePassword.classList.toggle('fa-eye-slash'); // Cambia el aspecto del ojito, al cambiar el input a tipo texto, el ojo aparece con una raya.
+}
+
+function verify() // Función para validar las contraseñas de registro de alumnos y las de modificación, también valida el D.N.I.
+{
+    let dnielement = document.getElementById("dni");
+    let dni = dnielement.value;
+    var numero, letra, letras;
+    var expresion_regular_dni = /^[XYZ]?\d{1,9}[A-Z]$/;
+    var pass = document.getElementById("pass1"); // pass es la ID del input pass0.
+    var pass2 = document.getElementById("pass2"); // pass2 es la ID del input pass1.
+
+    if(expresion_regular_dni.test(dni) === true)
+    {
+        numero = dni.substr(0, dni.length - 1);
+        numero = numero.replace('X', 0);
+        numero = numero.replace('Y', 1);
+        numero = numero.replace('Z', 2);
+        letra = dni.substr(dni.length - 1, 1);
+        numero = numero % 23;
+        // letras = 'TRWAGMYFPDXBNJZSQVHLCKET';
+        letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+        letras = letras.substring(numero, numero + 1);
+        if (letras != letra.toUpperCase())
+        {
+            toast(2, 'El D.N.I. o N.I.E. es Incorrecto', 'Verifica que los Números y la Letra o Letras Estén Bien.');
+            return false;
+        }
+        else
+        {
+            if (pass.value != pass2.value) // Verifico si los valores en los input pass y pass2 no coinciden.
+            {
+                toast(1, "Hay un Error", "Las contraseñas no coinciden, has escrito: " + pass.value + " y " + pass2.value); // Si no coinciden muestro error.
+                return false; // devulvo false, el formulario no se envía.
+            }
+            else // Si son iguales.
+            {
+                return true; // Devuelvo true, envía el formulario.
+            }
+        }
+    }
+    else
+    {
+        toast(2, 'El D.N.I. o N.I.E. es Incorrecto', 'Verifica que los Números y la Letra o Letras Estén Bien.');
+        return false;
+    }
+}
--- Programación/PHP/Ejercicio-2/pages/register.php
+++ Programación/PHP/Ejercicio-2/pages/register.php
@@ -1,3 +1,37 @@
 <?php
 echo "Perfect I am here.";
 ?>
+<h3>Registro de Clientes</h3>
+<br>
+<form action="logon.php" method="post" enctype="multipart/form-data" onsubmit="return verify()">
+    <label><input type="text" name="username" required> Nombre</label>
+    <br><br>
+    <label><input type="text" name="surname" required> Apellido1</label>
+    <br><br>
+    <label><input type="text" name="surname2"> Apellido2</label>
+    <br><br>
+    <label><input id="dni" type="text" name="dni" required> D.N.I.</label>
+    <br><br>
+    <label><input type="text" name="phone" required> Teléfono</label>
+    <br><br>
+    <label><input type="email" name="email" required> E-mail</label>
+    <br><br>
+    <label><input type="password" name="pass" id="pass1" onkeypress="showEye(1)" required> Contraseña</label>
+    <i onclick="spy(1)" class="far fa-eye" id="togglePassword1" style="margin-left: -140px; cursor: pointer; visibility: hidden;"></i>
+    <br><br>
+    <label><input type="password" id="pass2" onkeypress="showEye(2)" required> Repite Contraseña</label>
+    <i onclick="spy(2)" class="far fa-eye" id="togglePassword2" style="margin-left: -205px; cursor: pointer; visibility: hidden;"></i>
+    <br><br>
+    <label><input type="date" name="bday" required> Cumpleaños</label>
+    <br><br>
+    <fieldset>
+        <legend>Selecciona tu Género</legend>
+        <label><input type="radio" name="gender" value="0" checked required> Mujer</label>
+        <br><br>
+        <label><input type="radio" name="gender" value="1" required> Varón</label>
+        <br><br>
+    </fieldset>
+    <label><input type="file" name="profile"> Foto de Perfil<small>(opcional)</small></label>
+    <br><br>
+    <input type="submit" value="Regístrame!">
+</form>
