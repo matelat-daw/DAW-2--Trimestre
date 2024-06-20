c2d4af66dee260654b6fd568dcc86a91e4e00263
Orion68 <orions68@gmail.com>
2024-06-20 15:57:14 +0100

    Update 20-06-2024 15:57

---
+++ Projects/MultiLanguage/Language.php
@@ -0,0 +1,14 @@
+class Language
+{
+    private $language;
+
+    function __construct()
+    {
+        $this->language = $language;
+    }
+
+    function get_language()
+    {
+        return $this->language;
+    }
+}
---
+++ Projects/MultiLanguage/Spanish.php
@@ -0,0 +1,14 @@
+class Spanish
+{
+    private $index_h1;
+    private $index_h2;
+    private $index_h3;
+    private $index_h4;
+    private $index_h5;
+    private $index_h6;
+
+    function __construct()
+    {
+
+    }
+}
