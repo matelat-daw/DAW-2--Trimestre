59bf6bbb271429d7537d5c34b694eede3469eaa9
Orion68 <orions68@gmail.com>
2024-07-24 19:50:25 +0100

    Update 24-07-2024 19:50

---
+++ Projects/Angular/Angular-One/src/index.php
@@ -0,0 +1,22 @@
+<?php
+include "includes/conn.php";
+$title = "Página Principal";
+include "includes/header.php";
+?>
+<section class="container-fluid pt-3">
+    <div class="row">
+        <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <div id="view1" style="margin-bottom: 50%;">
+                    <br><br><br><br>
+                    <h1>Título de la Página.</h1>
+                    <br><br>
+                    <h3>Estas Viendo los usuarios de la WEB.</h3>
+                </div>
+            </div>
+        <div class="col-md-1"></div>
+    </div>
+</section>
+<?php
+include "includes/footer.html";
+?>
