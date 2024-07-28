311e50aa99cd26f758fab61042ef9eb3e1cb8f8f
Orion68 <orions68@gmail.com>
2024-07-28 14:02:02 +0100

    Update 28-07-2024 14:01

--- WEB/TEST/index.php
+++ WEB/TEST/index.php
@@ -7,12 +7,19 @@ include "includes/conn.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testing Things</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <?php
-        $sql = "INSERT INTO test VALUES(true);";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
+        // $sql = "INSERT INTO test VALUES(true);";
+        // $stmt = $conn->prepare($sql);
+        // $stmt->execute();
     ?>
+    <div style="padding: 5%;">
+        <span class="small">César Título</span>
+        <span class="med">123456</span>
+        <span class="large">7890</span>
+        <span class="xlarge">Ñandú</span>
+    </div>
 </body>
 </html>
