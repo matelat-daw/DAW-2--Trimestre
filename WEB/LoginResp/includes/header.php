9443d6233e4eaa160c8843d580b3a3935606f79a
Orion68 <orions68@gmail.com>
2024-08-05 21:30:02 +0100

    Update 5-8-2024 21:29

--- WEB/LoginResp/includes/header.php
+++ WEB/LoginResp/includes/header.php
@@ -6,7 +6,6 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style.css">
     <link href="css/base.css" rel="stylesheet" type="text/css">
     <link href="css/great.css" rel="stylesheet" type="text/css" media="screen and (min-width: 1025px)">
     <link href="css/medium.css" rel="stylesheet" type="text/css" media="screen and (min-width: 481px) and (max-width: 1024px)">
