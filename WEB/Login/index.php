90a75905ac16a9d068a513aca1ef1b429818770d
Orion68 <orions68@gmail.com>
2024-08-07 22:09:54 +0100

    Update 07-08-2024 22:10

--- WEB/Login/index.php
+++ WEB/Login/index.php
@@ -2,6 +2,29 @@
 $title = "Bienvenido Registarte para Usar el Sitio";
 include "includes/header.php";
 include "includes/modal.html";
+
+    function getIPAddress()
+    {
+        //whether ip is from the share internet.
+        if(!emptyempty($_SERVER['HTTP_CLIENT_IP']))
+        {
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        //whether ip is from the proxy.
+        elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR']))
+        {
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        //whether ip is from the remote address.
+        else
+        {
+                $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+$ip = getIPAddress();
+echo 'User Real IP Address - '. $ip;
+
 ?>
 <section class="container-fluid pt-3">
     <div class="row">
