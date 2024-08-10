abb37c8e1eecd7314ad4a1a4e0a7b2f86f4fe33b
Orion68 <orions68@gmail.com>
2024-08-10 13:04:50 +0100

    Update 10-08-2024 13:04

--- WEB/LoginResp/index.php
+++ WEB/LoginResp/index.php
@@ -11,7 +11,11 @@ include "includes/nav-pc-index.html";
                 <div id="view1">
                 <h2>Te Damos la Bienvenida al Sitio</h2>
                 <br>
-                    <div class="great medium mini"> <!-- Contenedor de la vista para resoluciones de pantalla media y grande, PC Tablet. -->
+                    <div class="great medium"> <!-- Contenedor de la vista para resoluciones de pantalla media y grande, PC Tablet. -->
+                        <div id="login"><?php include "includes/login.html"; ?></div> <!-- Contiene el html login.html. -->
+                        <div id="logon"><?php include "includes/logon.html"; ?></div> <!-- Contiene el html logon.html, es invisible al abrir la página. -->
+                    </div>
+                    <div class="mini"> <!-- Contenedor de la vista para resoluciones de pantalla media y grande, PC Tablet. -->
                         <div id="login"><?php include "includes/login.html"; ?></div> <!-- Contiene el html login.html. -->
                         <div id="logon"><?php include "includes/logon.html"; ?></div> <!-- Contiene el html logon.html, es invisible al abrir la página. -->
                     </div>
