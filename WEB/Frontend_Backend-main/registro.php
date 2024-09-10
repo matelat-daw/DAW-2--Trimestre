23abda0f29a24994ecec0004a944bd7cc27efd97
Orion68 <orions68@gmail.com>
2024-09-10 16:25:12 +0100

    Update 10-9-2024 16:25

---
+++ WEB/Frontend_Backend-main/registro.php
@@ -0,0 +1,15 @@
+<?php
+include("includes/head.php");
+include("includes/navregistro.php");
+include("includes/modal.html"); // Incluyo el dialogo para mostrar mensajes.
+
+?>
+<!-- Aquí empieza el cuerpo de la página de inicio -->
+<main>
+    <?php
+    include("includes/registro.php");
+    ?>
+
+    <?php
+    include("includes/footer.php");
+    ?>
