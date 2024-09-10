aed659864f7abdacf09b73ac4d630bd29320bb76
Orion68 <orions68@gmail.com>
2024-09-11 00:27:20 +0100

    Update 11-9-2024 0:27

---
+++ WEB/First_App/src/main/java/es/futureprograms/First_App/FirstAppApplication.java
@@ -0,0 +1,12 @@
+package es.futureprograms.First_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+	}
+}
---
+++ WEB/First_App/src/test/java/es/futureprograms/First_App/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.futureprograms.First_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
